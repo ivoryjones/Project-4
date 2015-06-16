12463218a7faedff133b1a2d515869ffaf143176
Ivory Jones <ivory.jones1@gmail.com>
2015-06-16 10:21:50 -0400

    optimize

--- views/js/main.js
+++ views/js/main.js
@@ -448,13 +448,17 @@ var resizePizzas = function(size) {
     return dx;
   }
 
-  // Iterates through pizza elements on the page and changes their widths
+//define pizzaContainerLength 
+var pizzaContainers = document.querySelectorAll(".randomPizzaContainer"); 
+  var dx = determineDx(pizzaContainers[0], size);  
+  var newwidth = (pizzaContainers[0].offsetWidth + dx) + 'px';
+  var pizzaContainerLength = pizzaContainers.length;
+
+  // moved variables outside the for loop
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    }
+    for (var i = 0; i < pizzaContainerLength; i++) {         
+        pizzaContainers[i].style.width = newwidth;
+      }
   }
 
   changePizzaSizes(size);
@@ -469,10 +473,11 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
+var pizzasDiv = document.getElementById("randomPizzas");
+  for (var i = 2; i < 100; i++) {
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
+//moved the variable outside the for loop
 
 // User Timing API again. These measurements tell you how long it took to generate the initial pizzas
 window.performance.mark("mark_end_generating");
@@ -502,10 +507,12 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
+  
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+ 
+    for (var i = 0; i < items.length; i++) {
+  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+      items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
@@ -528,7 +535,7 @@ document.addEventListener('DOMContentLoaded', function() {
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
-    elem.src = "images/pizza.png";
+    elem.src = "images/pizza.png"; //compressed img
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
